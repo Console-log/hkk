dbc9be00eab8510356afc79d9d0b7dd6cdfbb3bb
jiashaoteng <jiashaoteng@jd.com>
2018-09-08 17:29:05 +0800

    test

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,21 @@ router.get('/', function (req, res, next) {
   express.static('public')
 });
 
+router.get('/test', function (req, res, next) {
+  res.send([
+    {
+      name: '舒缓的起床音乐',
+      music: '../music/a1.mp3'
+    },
+    {
+      name: '胡一天叫你起床',
+      music: '../music/a2.mp3'
+    },
+    {
+      name: '白噪音',
+      music: '../music/a3.mp3'
+    },
+  ])
+});
+
 module.exports = router;
